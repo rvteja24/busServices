4a2d24b24bc418e03747e65fb064f7a3840ecc2e
Teja, Rayavarapu Vijaya <RayavarapuVijaya.Teja@fmr.com>
2020-03-30 14:21:36 +0530

    added script for populating mongodb

---
+++ mongoDbSetup/mongoTicketsCollectionSetup.js
@@ -0,0 +1,20 @@
+//Run this using the command "node mongoTicketsCollectionSetup.js" from command line while setting up the application on a new server with new mongoDb instance for the first time to populate the db with dummy data"
+const MongoClient = require('mongodb').MongoClient;
+const config = require('../config/config');
+const client = new MongoClient(config.uri, config.options);
+
+client.connect(err =>{
+    if(err) throw err;
+    console.log("connected!");
+    //client.db("bus-tickets").collection('tickets').deleteMany();
+    for(var i = 0; i < 40; i++){
+        var doc = {seatNumber: i+1, passengerAge: "22", passengerGender: "F", passengerName: "Jane Doe", ticketStatus:"CLOSED", phoneNumber:"9988776655", emailId:"abc@test.com"};
+
+        client.db(config.databaseName).collection(config.collectionName).insertOne( doc, function(err, result){
+            if (err) throw err;
+            console.log(result);
+        });
+
+    };
+    //client.close();
+});
