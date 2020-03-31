49755e62a7f80c31a50f6c4563ff1711761298d1
Teja, Rayavarapu Vijaya <RayavarapuVijaya.Teja@fmr.com>
2020-03-31 19:49:08 +0530

    updated port to 3000

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Bus ticketing service APIs'));
 require('./app/routes/routes')(app);
 app.listen(port, () => console.log(`Bus services listening on port ${port}!`))
 
